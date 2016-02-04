42f637264ed8c0bc3b50f51c7a7ff25732f1f38b
Frank <socieboy@gmail.com>
2016-02-04 15:01:40 -0800

    Fix logout

--- src/Controllers/AuthController.php
+++ src/Controllers/AuthController.php
@@ -30,7 +30,7 @@ class AuthController extends Controller
      */
     public function __construct()
     {
-        $this->middleware('guest', ['except' => 'logout']);
+        $this->middleware('guest', ['except' => 'getLogout']);
         $this->loginView = route('CMS::admin.login');
         $this->redirectTo = route('CMS::admin.home');
     }
